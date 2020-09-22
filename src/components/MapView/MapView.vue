b0f8d2a909ca51cd02ccd7f8a7f3224fe552d196
nvdai2401 <nvdai2401@gmail.com>
2020-09-22 22:11:42 +0700

    add google map and integrate with api

---
+++ src/components/MapView/MapView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    MapView
+    <GmapMap
+      style="width: 600px; height: 300px;"
+      :zoom="1"
+      :center="{ lat: 0, lng: 0 }"
+      :options="{
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: true,
+        disableDefaultUI: false,
+      }"
+    >
+      <GmapMarker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker.position"
+      />
+      <GmapMarker
+        v-if="this.place"
+        label="★"
+        :position="{
+          lat: this.place.geometry.location.lat(),
+          lng: this.place.geometry.location.lng(),
+        }"
+      />
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapView',
+  data() {
+    return {
+      markers: [],
+      place: null,
+    };
+  },
+};
+</script>
